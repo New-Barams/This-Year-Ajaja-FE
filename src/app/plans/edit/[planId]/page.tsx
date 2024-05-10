e7410963b0aae3604ee9cf9a41563bf321c173c7
nocheol <qkdl60@gmail.com>
2024-05-10 17:25:48 +0900

    style : scroll 수정

--- src/app/plans/edit/[planId]/page.tsx
+++ src/app/plans/edit/[planId]/page.tsx
@@ -37,127 +37,131 @@ export default function EditPage({ params }: { params: { planId: string } }) {
   } = usePlanEditPage(params.planId);
 
   return (
-    <div className={classNames('edit-plan-content')}>
-      <div className="edit-plan-content__breadcrumb font-size-base color-origin-text-100">
-        <Link href="/home">홈</Link>
-        &gt;
-        <Link href={`/plans/${planId}`}>계획</Link>
-        &gt;
-        <span>계획 수정</span>
-      </div>
-      <div className="edit-plan-content__main">
-        <div className="edit-plan-content__icon">
-          <button
-            onClick={() => {
-              setIsSelectIconModalOpen(true);
-            }}>
-            <Image
-              src={`/animal/${planIcons[planContent.iconNumber]}.png`}
-              alt={`${planIcons[planContent.iconNumber]}`}
-              width={50}
-              height={50}
-              priority
-            />
-          </button>
-          <span className="font-size-xs">아이콘을 클랙해 변경할 수 있어요</span>
+    <div className={classNames('edit-plan-page')}>
+      <div className="edit-plan-page__main">
+        <div className="edit-plan-page__breadcrumb font-size-base color-origin-text-100">
+          <Link href="/home">홈</Link>
+          &gt;
+          <Link href={`/plans/${planId}`}>계획</Link>
+          &gt;
+          <span>계획 수정</span>
         </div>
-        <div className="edit-plan-content__plan">
-          <PlanInput
-            classNameList={['create-plan-content__plan--title']}
-            editable={true}
-            kind="title"
-            placeholder="어떤 계획을 가지고 계신가요?"
-            onChangeInput={handleChangeTitle}
-            maxLength={20}
-            textInput={planContent.title}
-            nextTextAreaRef={nextTextAreaRef}
-          />
-
-          <PlanInput
-            classNameList={['create-plan-content__plan--description']}
-            editable={true}
-            kind="content"
-            placeholder="계획에 대해서 자세히 설명해주세요!"
-            onChangeInput={handleChangeDescription}
-            maxLength={250}
-            textInput={planContent.description}
-            nextTextAreaRef={nextTextAreaRef}
-          />
-        </div>
-
-        <div className={classNames('edit-plan-content__tag')}>
-          <div className="edit-plan-content__tag--input">
-            <TagInput
-              disabled={planContent.tags.length === 5}
-              onSubmit={handleAddTag}
-              placeholder="태그를 입력해주세요!"
+        <div className="edit-plan-page__content">
+          <div className="edit-plan-page__icon">
+            <button
+              onClick={() => {
+                setIsSelectIconModalOpen(true);
+              }}>
+              <Image
+                src={`/animal/${planIcons[planContent.iconNumber]}.png`}
+                alt={`${planIcons[planContent.iconNumber]}`}
+                width={50}
+                height={50}
+                priority
+              />
+            </button>
+            <span className="font-size-xs">
+              아이콘을 클랙해 변경할 수 있어요
+            </span>
+          </div>
+          <div className="edit-plan-page__plan">
+            <PlanInput
+              classNameList={['create-plan-content__plan--title']}
+              editable={true}
+              kind="title"
+              placeholder="어떤 계획을 가지고 계신가요?"
+              onChangeInput={handleChangeTitle}
+              maxLength={20}
+              textInput={planContent.title}
+              nextTextAreaRef={nextTextAreaRef}
             />
-            <span
-              className={classNames(
-                'counter',
-                'font-size-xs',
-                planContent.tags.length === 5 && 'color-origin-primary',
-              )}>{`(${planContent.tags.length}/5)`}</span>
+
+            <PlanInput
+              classNameList={['create-plan-page__plan--description']}
+              editable={true}
+              kind="content"
+              placeholder="계획에 대해서 자세히 설명해주세요!"
+              onChangeInput={handleChangeDescription}
+              maxLength={250}
+              textInput={planContent.description}
+              nextTextAreaRef={nextTextAreaRef}
+            />
           </div>
 
-          <div className="edit-plan-content__tag--tags">
-            {planContent.tags.map((tag, index) => (
-              <DeletableTag
-                key={index}
+          <div className={classNames('edit-plan-page__tag')}>
+            <div className="edit-plan-content__tag--input">
+              <TagInput
+                disabled={planContent.tags.length === 5}
+                onSubmit={handleAddTag}
+                placeholder="태그를 입력해주세요!"
+              />
+              <span
+                className={classNames(
+                  'counter',
+                  'font-size-xs',
+                  planContent.tags.length === 5 && 'color-origin-primary',
+                )}>{`(${planContent.tags.length}/5)`}</span>
+            </div>
+
+            <div className="edit-plan-page__tag--tags">
+              {planContent.tags.map((tag, index) => (
+                <DeletableTag
+                  key={index}
+                  onClick={() => {
+                    handleRemoveTag(tag);
+                  }}>
+                  {tag}
+                </DeletableTag>
+              ))}
+            </div>
+            <AjajaButton
+              ajajaCount={planData.ajajas}
+              isFilled={planData.writer.ajajaPressed}
+              disabled
+            />
+          </div>
+          <div className="edit-plan-page__switches">
+            <div className={classNames('edit-plan-content__switches--public')}>
+              <IconSwitchButton
+                onIconName="PLAN_OPEN"
+                offIconName="PLAN_CLOSE"
+                isActive={planContent.isPublic}
                 onClick={() => {
-                  handleRemoveTag(tag);
-                }}>
-                {tag}
-              </DeletableTag>
-            ))}
-          </div>
-          <AjajaButton
-            ajajaCount={planData.ajajas}
-            isFilled={planData.writer.ajajaPressed}
-            disabled
-          />
-        </div>
-        <div className="edit-plan-content__switches">
-          <div className={classNames('edit-plan-content__switches--public')}>
-            <IconSwitchButton
-              onIconName="PLAN_OPEN"
-              offIconName="PLAN_CLOSE"
-              isActive={planContent.isPublic}
-              onClick={() => {
-                handleChangeIsPublic(!planContent.isPublic);
-              }}
-            />
-            <span className={classNames('font-size-xs')}>
-              {planContent.isPublic ? '계획 공개' : '계획 비공개'}
-            </span>
-            <HelpButton
-              helpText={`계획 공개를 하면 둘러보기에서\n모든 사람들이 볼 수 있어요.`}
-            />
-          </div>
+                  handleChangeIsPublic(!planContent.isPublic);
+                }}
+              />
+              <span className={classNames('font-size-xs')}>
+                {planContent.isPublic ? '계획 공개' : '계획 비공개'}
+              </span>
+              <HelpButton
+                helpText={`계획 공개를 하면 둘러보기에서\n모든 사람들이 볼 수 있어요.`}
+              />
+            </div>
 
-          <div className={classNames('edit-plan-content__switches--can-ajaja')}>
-            <IconSwitchButton
-              onIconName="NOTIFICATION_ON"
-              offIconName="NOTIFICATION_OFF"
-              isActive={planContent.canAjaja}
-              onClick={() => {
-                handleChangeCanAjaja(!planContent.canAjaja);
-              }}
-            />
-            <span className={classNames('font-size-xs')}>
-              {planContent.canAjaja
-                ? '매주 월요일 18:00시 마다 응원 메세지 알림 활성화'
-                : '응원 메세지 알림 비활성화'}
-            </span>
-            <HelpButton
-              helpText={`매주 몇 명의 새로운 사람들이 내 계획에\n아좌좌를 눌러 응원했는지 알려드려요.`}
-              textPosition={planContent.canAjaja ? 'top-left' : 'top'}
-            />
+            <div className={classNames('edit-plan-page__switches--can-ajaja')}>
+              <IconSwitchButton
+                onIconName="NOTIFICATION_ON"
+                offIconName="NOTIFICATION_OFF"
+                isActive={planContent.canAjaja}
+                onClick={() => {
+                  handleChangeCanAjaja(!planContent.canAjaja);
+                }}
+              />
+              <span className={classNames('font-size-xs')}>
+                {planContent.canAjaja
+                  ? '매주 월요일 18:00시 마다 응원 메세지 알림 활성화'
+                  : '응원 메세지 알림 비활성화'}
+              </span>
+              <HelpButton
+                helpText={`매주 몇 명의 새로운 사람들이 내 계획에\n아좌좌를 눌러 응원했는지 알려드려요.`}
+                textPosition={planContent.canAjaja ? 'top-left' : 'top'}
+              />
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="edit-plan-content__bottom">
+      <div className="edit-plan-page__bottom">
         <Button
           color="white-100"
           background="primary"
